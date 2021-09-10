a3aaa0470f3d02ec3e99126317fdf475b7d3335d
Vanya0908 <89701908+Vanya0908@users.noreply.github.com>
2021-09-10 17:12:11 +0300

    Add files via upload

---
+++ 1.1.py
@@ -0,0 +1,63 @@
+#создать перменную типа String
+name_1 = 'Vania'
+print(name_1, type(name_1))
+
+#создать переменную типа Integer
+name_2 = 28
+print(name_2, type(name_2))
+
+#создать переменную типа Float
+name_3 = 78.9
+print(name_3, type(name_3))
+
+#создать переменную типа Bytes
+name_4 = bytes ([40,50,60,70])
+print(name_4, type(name_4))
+
+#создать переменную типа List
+name_5 = [0 for i in range(5)]
+print(name_5, type(name_5))
+
+#создать переменную типа Tuple
+name_6 = (40,60,120.5,'А')
+print(name_6, type(name_6))
+
+#создать переменную типа Set
+name_7 = {1,2,3,4}
+print(name_7, type(name_7))
+
+#создать переменную типа Frozenset
+name_8 = frozenset ({40,60,25})
+print(name_8, type(name_8))
+
+#создать переменную типа Dict
+name_9 = dict (minsk = 7,grodno = 4,gomel = 3)
+print(name_9, type(name_9))
+
+#создать две переменные типа String,создать перменную в которой суммируете эти переменные
+a = 'Ivan'
+b = 'Lera'
+c = 'a+b'
+print(c)
+
+#создать две переменные типа Integer,создать переменную в которой суммируете эти переменные
+g = 20
+h = 10
+f = g+h
+print(f)
+
+#создать перменную в которой разделите эти переменные Integer
+w = g / h
+print(w)
+
+#создать переменную в которой умножите эти переменные integer
+t = f * w
+print(t)
+
+#создать переменную в которой разделите без остатка эти переменные Integer
+d = t / w
+print(d)
+
+#создать переменную которой надо присвоить остаток от деления этих переменных Integer
+k = 30
+print(k)
